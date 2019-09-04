f0b225b35b6a58516525b27fdeac325a41fbe523
Fernando Gomes <fcgomes.92@gmail.com>
2019-09-04 10:31:25 -0300

    first commit. ref: https://medium.com/@adityadroid/60-days-of-flutter-day-3-4-building-a-chat-screen-in-flutter-e2ed36388dc7

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/ConversationPageList.dart';
+
+void main() => runApp(Messio());
+
+class Messio extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drinks',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ConversationPageList(),
+    );
+  }
+}
